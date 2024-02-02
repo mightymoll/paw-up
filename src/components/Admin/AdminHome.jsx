f2e4b949445f0aa9e5822e7493e429af3d4603e5
molly <mnfelts@gmail.com>
2024-02-02 09:45:05 +0100

    AdminHome - protect contents
    - use effect and state to control what user sees based on their accessLevel (admin, member or public)
    - user is notified if they do not have access & shown a link to login page

--- src/components/Admin/AdminHome.jsx
+++ src/components/Admin/AdminHome.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 
 function AdminHome() {
 
   const navigate = useNavigate();
 
-  // get user information by decoding JWT token & use to control content
-  const user = axios.get('http://localhost:5001/getJWT');
+  const [accessLevel, setAccessLevel] = useState('public');
+  const [state, setState] = useState('');
+
+  useEffect(() => {
+    setState('loading');
+    // get user information by decoding JWT token & use to control content
+    axios.get('http://localhost:5001/getJWT')
+      .then((res) => {
+        setState('success');
+        // use access value from response to control page contents
+        setAccessLevel(res.data.access)
+      })
+      .catch((err) => {
+        console.error('Error:', err);
+        setState('error');
+      });
+  }, []);
+
+  // 
+
+  while (state === 'loading') {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
 
   // TO DO: create individual pages  
-  
   const navigateAddAnimal = () => {
     navigate('/newAnimal');
   };
@@ -23,12 +47,24 @@ function AdminHome() {
     navigate('/editAsso');
   };
 
-  return (
-    <div>
+  if (state === 'success') {
+
+    // show error message if there is an error recuperating user info
+    if (state === 'error') {
+      return (
+        <div>
+          Une erreur est servenue...
+        </div>
+      )
+    }
+
+    return (
+      (accessLevel === "admin" || accessLevel === 'member') ?
+        <div>
       <h1>Admin Dashboard</h1>
       <button onClick={navigateAddAnimal}>Ajouter un Animal</button>
       {/* if user has 'admin' access show additional functions */}
-      {(user.access === "admin") ? (
+          {(accessLevel === "admin") ? (
         <div>
           <button onClick={navigateUsersList}>Gestion utilisaters</button>
           <button onClick={navigateEditAsso}>Modifier les infos d'assosciation</button>
@@ -36,8 +72,14 @@ function AdminHome() {
         <></>}
       <h2>Liste des Animaux :</h2>
       {/* to do : list all animals with modify/delete buttons */}
-    </div >
+        </div>
+        :
+        <div>
+          {/* if user does NOT have admin or member access, don't display contents */}
+          <Link to="/login">Vous n'avez pas access au ce page;<br></br> veuillez connecter en cliquant ici</Link>
+        </div>
   )
+  }
 }
 
-export default AdminHome
+export default AdminHome;
