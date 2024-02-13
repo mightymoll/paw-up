d4fc43426fdea038862f94426252e66ece7af8b4
molly <mnfelts@gmail.com>
2024-02-13 10:04:03 +0100

    useData hook
    - fetch data hook to use throughout website
    - applied to 'Header' component to show asso name & logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './header.scss'
 import NavBar from './NavBar'
-import logo from '../../assets/logo.png';
+import pawUpLogo from '../../assets/logo.png';
+import useData from '../../utils/useData'
 
 
 // placeholder
 function Header() {
-  // TODO: replace with title entered in Admin dashboard
-  const title = "Name of Association";
+
+  const { data, error, isLoaded } = useData(
+    "http://localhost:5001/asso/65c63c6dda87a502bdf4cb45"
+  );
+
+  if (error !== null) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
+    !isLoaded ? <div>...loading</div> :
     <div className="header">
       <div className="assoInfo">
-        {/* TODO: update with admin-uploaded logo if provided */}
-        <img className="logo" src={logo} alt="PawUp logo" />
-        <h1>{title}</h1>
+          {/* show uploaded logo if provided, else use placeholder 'paw up' logo */}
+          <img className="logo" src={data.logo ? data.logo : pawUpLogo} alt="PawUp logo" />
+          <h1>{data.name}</h1>
       </div>
       <NavBar />
     </div>
