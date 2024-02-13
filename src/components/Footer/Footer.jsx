b3c609a8f1415b79e71f09274d64782a0dc053d3
molly <mnfelts@gmail.com>
2024-02-13 11:16:59 +0100

    footer w/values
    - updated footer with values from API regarding asso

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,35 +2,38 @@ import React from 'react';
 import './footer.scss';
 import facebook from '../../assets/facebook.png';
 import instagram from '../../assets/intagram.png';
+import useData from '../../utils/useData'
 
-// placeholder
 function Footer() {
 
-  // TODO: replace with data entered in Admin dashboard
-  var title = "Name of Association";
+  // get all data related to association
+  const { data, error, isLoaded } = useData(
+    "http://localhost:5001/asso/65c63c6dda87a502bdf4cb45"
+  );
 
-  var phone = '06 68 01 99 38';
-  var addressStreet = '123 rue du Toits';
-  var addressCity = 'Marly-en-ville 93209';
-  var email = 'assoname@orange.fr';
+  if (error !== null) {
+    return <div>Error: {error.message}</div>;
+  }
 
   var socials = [
-    { name: 'facebook', url: 'https://facebook.com', icon: facebook },
-    { name: 'instagram', url: 'https://instagram.com', icon: instagram }
+    { name: 'facebook', url: data.soc_fb, icon: facebook },
+    { name: 'instagram', url: data.soc_insta, icon: instagram }
   ];
 
   return (
+    !isLoaded ? <div>...loading</div> :
     <div className="footer" >
       <div className="contact">
-        <p>{phone}</p>
+          <p>{data.tel}</p>
         <div className="address">
-          <p>{addressStreet}</p>
-          <p>{addressCity}</p>
+            <p>{data.loc_street}</p>
+            <p>{data.loc_city}</p>
+            <p>{data.loc_postal}</p>
         </div>
-        <a href={'mailto:' + { email }}><p>{email}</p></a>
+          <a href={'mailto:' + data.email}><p>{data.email}</p></a>
       </div>
       <div className="general">
-        <h3>{title}</h3>
+          <h3>{data.name}</h3>
         <p>copyright ©2023 MVF</p>
       </div>
       <div className="social">
@@ -38,6 +41,7 @@ function Footer() {
           <a key={social.name} href={social.url}>
             <img src={social.icon} alt={social.name + " logo"} /></a>
         )}
+          {data.soc_other ? <a key={data.soc_other} href={data.soc_other}>{data.soc_other_name}</a> : <></>}
       </div>
     </div >
   );
