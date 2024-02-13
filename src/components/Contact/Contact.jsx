098f27888c3577cb8a315c5c75b9e84a10594103
molly <mnfelts@gmail.com>
2024-02-13 12:24:03 +0100

    contact form - send email
    - connected to backed to send email
    - alerts status after mail is sent
    - resets form if successful

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,27 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contact.scss';
+import axios from 'axios'
 
 // placeholder
 function Contact() {
+  const [status, setStatus] = useState('')
+
+  const handleSubmitForm = async e => {
+    e.preventDefault()
+    setStatus("Sending...")
+    const { name, email, subject, message } = e.target.elements
+    let details = {
+      name: name.value,
+      email: email.value,
+      subject: subject.value,
+      message: message.value
+    }
+    console.log(details)
+    await axios.post("http://localhost:5001/sendMail", details)
+      .then((res) => {
+        console.log(res)
+        setStatus(res.data)
+
+        if (status === 'Votre message a été envoyé! Merci') {
+          document.getElementById("contactForm").reset();
+        }
+      })
+  }
+
   return (
     <div className='contactUs'>
       <h3>contacter nous</h3>
-      <form action="" name="contactForm" id="contactForm">
+      <form onSubmit={handleSubmitForm} id="contactForm">
         <div className="userInfo">
-          <div className="field">
-            <label htmlFor="name">nom + prénom :</label>
-            <input type="text" name="userName" id="name" required />
+          <div className='form-group'>
+            <label>nom + prénom :</label>
+            <input type="text" name="name" required />
           </div>
-          <div className="field">
-            <label htmlFor="email">email :</label>
-            <input type="text" name="userEmail" id="email" required />
+          <div className='form-group'>
+            <label>email :</label>
+            <input type="text" name="email" required />
           </div>
         </div>
-        <div className="field message">
-          <label htmlFor="message">votre message :</label>
-          <textarea name="message" id="message" required />
+        <div className='form-group'>
+          <label>Subjet :</label>
+          <input type="text" name="subject" />
         </div>
-        <div id="controlButton" className="btn" role="button">ENVOYER</div>
+        <div className='form-group'>
+          <label>Votre message :</label>
+          <textarea name="message" required />
+        </div>
+        <p>{status}</p>
+        <button className="btn" type="submit">ENVOYER</button>
       </form>
     </div>
   );
